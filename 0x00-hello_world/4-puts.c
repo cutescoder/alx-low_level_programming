155250eed149df37131774e754c8b304383972c7
cutescoder <jubabanjo@gmail.com>
2023-08-10 04:38:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main: A program that prints and a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
