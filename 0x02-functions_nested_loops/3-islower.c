067c6d577a7848000adb12a8001104d6c52af6da
cutescoder <jubabanjo@gmail.com>
2023-08-16 18:30:29 +0100

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,17 +3,18 @@
 /**
  * _islower - A function that checks for lowercase character
  * @c: main character
- * Returns 1 if c is lowercase, Returns 0 if otherwise
+ *
+ * Return: 1 if c is lowercase, Returns 0 if otherwise
  */
 
 int _islower(int c)
 {
 
 	if (c >= 'a' && c <= 'z')
-		return (1);
+		return (1); /* c is a lowercase character */
 
 	else
 	{
-		return (0);
+		return (0); /* c is not a lowercase character */
 	}
 }
