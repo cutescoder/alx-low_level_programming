3458db99c99d107a01c66f8bff760100f3394a2e
cutescoder <jubabanjo@gmail.com>
2023-08-17 17:13:37 +0100

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - A function that computes value of an integer
+ * @n: main character
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+
+}
