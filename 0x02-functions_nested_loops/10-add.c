858b793ee56bc6f81256d5c89f5fdadd21cf8c86
cutescoder <jubabanjo@gmail.com>
2023-08-18 02:40:11 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - A function that adds two integers
+ * @a: first number
+ * @b: second number
+ * Return: Result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table -  A function that prints the 9 times table, starting with 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+
+}
