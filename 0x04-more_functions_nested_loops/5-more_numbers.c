f69c197192d2c360e38d956535bb186973c8ae46
cutescoder <juniortinto@yahoo.com>
2023-10-23 21:04:44 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - A function that prints more numbers
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	int num;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			num = j;
+			if (num >= 10)
+			{
+
+			_putchar('0' + (num / 10));
+
+			num %= 10;
+
+			}
+			_putchar('0' + num);
+		}
+		_putchar('\n');
+	}
+}
